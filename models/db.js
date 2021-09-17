3630d9af36ddfe3b415453d4f263e8744c6e3b69
Basim <basim.abdullah247@gmail.com>
2021-09-17 18:09:57 +0500

    fix

--- models/db.js
+++ models/db.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 const mongoose = require('mongoose');
 
 /*mongoose.connect('mongodb://localhost:27017/ExpenseDB', { useNewUrlParser: true }, (err) => {
     if (!err) console.log("Connection Successful");
     else console.log("Error: " + err);
 });*/
-mongoose.connect('mongodb+srv://Basim:7VVmKuPeQheHuAD@expense-manager.knex1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
+mongoose.connect(`mongodb+srv://Basim:7VVmKuPeQheHuAD@expense-manager.knex1.mongodb.net/expense-manager?retryWrites=true&w=majority`, { useNewUrlParser: true }, (err) => {
     if (!err) console.log("Connection Successful");
     else console.log("Error: " + err);
 });
